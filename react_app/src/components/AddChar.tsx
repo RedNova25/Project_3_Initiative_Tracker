7d838e292292c1871e6e9ec1570f2ab3c06d5841
RedNova25 <30453795+RedNova25@users.noreply.github.com>
2026-02-09 17:09:03 -0500

    changed component names

--- react_app/src/components/AddChar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddChar = () => {
-  return (
-    <div>AddChar</div>
-  )
-}
-
-export default AddChar
---
+++ react_app/src/components/ViewCharDB.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewCharDB = () => {
+  return (
+    <div>ViewCharDB</div>
+  )
+}
+
+export default ViewCharDB
